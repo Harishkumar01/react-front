5a4f7632c430b416af65a1d18cf99fd26eebd41b
harish <harishkumarvijayarajan@gmail.com>
2020-04-23 19:12:00 +0530

    home edit

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import Posts from '../posts/Posts';
 import {Link} from 'react-router-dom';
-import homesta from '../Images/homsta.jpg'
+import homesta from '../Images/homesta.jpg'
 
 const Home = () => (
     <div>
         <div className="jumbotron bg-muted text-body">
             <h2>Welcome</h2>
-            <img src= {homesta} />
             <p className = "lead">Hello Cricket Freaks</p>
         </div>
         <div className="container">
