b186b52e0c6c002ab73797780f20946f44340c7e
harish <harishkumarvijayarajan@gmail.com>
2020-04-23 18:40:06 +0530

    adding image

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Posts from '../posts/Posts';
 import {Link} from 'react-router-dom';
+import homesta from './Images/homsta.jpg'
 
 const Home = () => (
     <div>
         <div className="jumbotron bg-muted text-body">
             <h2>Welcome</h2>
+            <img src= {homesta} />
             <p className = "lead">Hello Cricket Freaks</p>
         </div>
         <div className="container">
