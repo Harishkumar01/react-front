c7b3352097508c518d5737e4bdf5ae9f1aceeb0f
harish <harishkumarvijayarajan@gmail.com>
2020-04-23 13:05:45 +0530

    edit home

--- src/core/Home.js
+++ src/core/Home.js
@@ -5,8 +5,8 @@ import {Link} from 'react-router-dom';
 const Home = () => (
     <div>
         <div className="jumbotron bg-muted text-body">
-            <h2>Hello</h2>
-            <p className = "lead">Welcome Cricket Freaks</p>
+            <h2>Welcome</h2>
+            <p className = "lead">Hello Cricket Freaks</p>
         </div>
         <div className="container">
         <Link className="btn btn-raised btn-info mr-5" to={`/post/create`}>
